053ace85c92ecebfdad39d711c64b6b063c42291
Asrarengiz <esrarengiz.agazade@gmail.com>
2022-05-21 15:09:27 +0400

    Add files via upload

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="wrapper">
+      <span>No content</span>
+    </div>
+  );
+};
+
+export default Loader;
